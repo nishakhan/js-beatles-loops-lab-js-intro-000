b289d858990b1f43dc219a48c73ecf29ef443c1f
Nisha Khan <nisha.f.khan@gmail.com>
2020-07-31 02:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = []
+  for (var counter = 0; counter < musicians.length; counter++) {
+    emptyArray.push(musicians[counter] + " plays " + instruments[counter])
+  }
+  return emptyArray
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+  while (i < facts.length) {
+    i++;
+    return i + " !!! ";
+    }
+}
