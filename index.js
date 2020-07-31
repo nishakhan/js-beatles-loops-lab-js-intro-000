0bac32eafe03089e76472110a520d93f5dd4614a
Nisha Khan <nisha.f.khan@gmail.com>
2020-07-31 02:32:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function johnLennonFacts(facts) {
     }
   return emptyArray
 }
+
+function iLoveTheBeatles(num) {
+  var emptyArray = []
+  do {
+    emptyArray.push("I love the Beatles!");
+    num++
+  } while (num < 15)
+  return emptyArray;
+}
