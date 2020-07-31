917b8185b96802da98122466f1991b42ffc7c56b
Nisha Khan <nisha.f.khan@gmail.com>
2020-07-31 02:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
   return emptyArray
 }
 
-function johnLennonFacts() {
-  const facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
-  ];
+function johnLennonFacts(facts) {
   var emptyArray = []
   let i = 0
   while (i < facts.length) {
