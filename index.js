2fdb6d98b9920f487a6777ed60fba7963a4ba723
Nisha Khan <nisha.f.khan@gmail.com>
2020-07-31 02:07:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts(facts) {
   ];
   var emptyArray = []
   while (i < facts.length) {
-    emptyArray.push(facts[i] + " !!! ";
+    emptyArray.push(facts[i] + " !!! ");
     i++;
     }
   return facts
